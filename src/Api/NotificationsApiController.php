59d82fbe9c9ff3cf10471a38bf13f81b2a1d6100
Sergey Populov <sergey@saritasa.com>
2017-04-13 20:33:48 +0500

    DocStrings for NotificationsApiServiceProvider

--- src/Api/NotificationsApiController.php
+++ src/Api/NotificationsApiController.php
@@ -3,12 +3,14 @@
 namespace Saritasa\PushNotifications\Api;
 
 use Saritasa\DingoApi\BaseApiController;
+use Saritasa\PushNotifications\Models\Notification;
+use Saritasa\Transformers\BaseTransformer;
 
 class NotificationsApiController extends BaseApiController
 {
     public function getUserNotifications()
     {
-
+        return $this->json(new Notification(), new BaseTransformer());
     }
 
     public function markNotificationsAsViewed()
--- src/NotificationsApiServiceProvider.php
+++ src/NotificationsApiServiceProvider.php
@@ -5,6 +5,10 @@ namespace Saritasa\PushNotifications;
 use Dingo\Api\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * Register URLs, handed by this package
+ * and declare artifacts, that can be published to application (config, DB migrations, swagger definitions)
+ */
 class NotificationsApiServiceProvider extends ServiceProvider
 {
     /**
@@ -22,50 +26,21 @@ class NotificationsApiServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->declarePublishedConfig();
-            $this->declarePublishedArtifacts();
-            $this->declarePublishedMigrations();
+            $this->declarePublishedFiles();
         }
 
         $this->registerRoutes();
     }
 
     /**
-     * Register the service provider.
-     *
-     * @return void
+     * Register API routes and handlers for them
      */
-    public function register()
-    {
-    }
-
-    private function declarePublishedConfig()
-    {
-        $this->publishes([
-            __DIR__.'/../config/push.php' => config_path('push.php')
-        ], 'config');
-    }
-
-    private function declarePublishedArtifacts()
-    {
-        $this->publishes([
-            __DIR__.'/../Artifacts/API' => base_path('Artifacts/API')
-        ], 'swagger');
-    }
-
-    private function declarePublishedMigrations()
-    {
-        $this->publishes([
-            __DIR__.'/../database' => database_path()
-        ], 'migrations');
-    }
-
-    private function registerRoutes()
+    protected function registerRoutes()
     {
         /* @var Router $router */
         $router = $this->app['api.router'];
         $router->version('v1', [
-            'middleware' => ['api', 'api.auth'],
+            'middleware' => ['api'],
             'namespace' => 'Saritasa\PushNotifications\Api'
         ], function(Router $router) {
             $this->registerSettingsRoutes($router);
@@ -73,7 +48,7 @@ class NotificationsApiServiceProvider extends ServiceProvider
         });
     }
 
-    private function registerSettingsRoutes(Router $router)
+    protected function registerSettingsRoutes(Router $router)
     {
         $router->group([ 'prefix' => 'settings' ], function(Router $router) {
 
@@ -105,5 +80,37 @@ class NotificationsApiServiceProvider extends ServiceProvider
             ]);
 
         });
+    }
+
+    /**
+     * Declare which files can be published and customized in main application:
+     * config, swagger declarations, DB migrations
+     */
+    protected function declarePublishedFiles()
+    {
+        $this->declarePublishedConfig();
+        $this->declarePublishedArtifacts();
+        $this->declarePublishedMigrations();
+    }
+
+    private function declarePublishedConfig()
+    {
+        $this->publishes([
+            __DIR__.'/../config/push.php' => config_path('push.php')
+        ], 'config');
+    }
+
+    private function declarePublishedArtifacts()
+    {
+        $this->publishes([
+            __DIR__.'/../Artifacts/API' => base_path('Artifacts/API')
+        ], 'swagger');
+    }
+
+    private function declarePublishedMigrations()
+    {
+        $this->publishes([
+            __DIR__.'/../database' => database_path()
+        ], 'migrations');
     }
 }
