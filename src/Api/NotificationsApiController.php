f823cdf04b3752480d6687f89c8bb0d4d9fc8d46
Sergey Populov <sergey@saritasa.com>
2017-04-20 05:48:59 +0500

    Test sending notification

--- src/Api/NotificationsApiController.php
+++ src/Api/NotificationsApiController.php
@@ -2,19 +2,24 @@
 
 namespace Saritasa\PushNotifications\Api;
 
+use Dingo\Api\Http\Request;
 use Saritasa\DingoApi\BaseApiController;
-use Saritasa\PushNotifications\Models\Notification;
-use Saritasa\Transformers\BaseTransformer;
+use Saritasa\DingoApi\Traits\CurrentApiUserTrait;
+use Saritasa\PushNotifications\NotificationTransformer;
 
 class NotificationsApiController extends BaseApiController
 {
+    use CurrentApiUserTrait;
+
     public function getUserNotifications()
     {
-        return $this->json(new Notification(), new BaseTransformer());
+        return $this->json($this->user()->unreadNotifications, new NotificationTransformer());
     }
 
-    public function markNotificationsAsViewed()
+    public function markNotificationsAsViewed(Request $request)
     {
-
+        $ids = $request->get('notification_ids');
+        $notifications = $this->user()->unreadNotifications->whereIn('id', $ids);
+        $notifications->markAsRead();
     }
 }
---
+++ src/NotificationTransformer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Saritasa\PushNotifications;
+
+use Saritasa\Transformers\BaseTransformer;
+
+/**
+ * Transform Laravel notification, stored in Database into API structure
+ */
+class NotificationTransformer extends BaseTransformer
+{
+
+}
