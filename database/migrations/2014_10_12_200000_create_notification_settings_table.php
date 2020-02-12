374954b9a61e28a20e8e18d8b347100f8f1cc2b2
Sergey Populov <populov@gmail.com>
2020-02-12 17:34:08 +0500

    Fix duplicated migration class name

--- database/migrations/2014_10_12_200000_create_notification_settings_table.php
+++ database/migrations/2014_10_12_200000_create_notification_settings_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNotificationTypesTable extends Migration
+class CreateNotificationSettingsTable extends Migration
 {
     /**
      * Run the migrations.
