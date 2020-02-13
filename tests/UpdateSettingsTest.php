5789cfb4f51762fe8c85e7a0a20865f8b51b49c5
Sergey Populov <sergey@saritasa.com>
2020-02-13 13:38:30 +0500

    Example unit test

---
+++ tests/UpdateSettingsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Saritasa\PushNotifications\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class UpdateSettingsTest extends TestCase
+{
+    public function testExample()
+    {
+        static::assertTrue(true);
+    }
+}
