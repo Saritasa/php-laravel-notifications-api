282b654975fadd9b082bb2f68836d583fb7e2ba8
Ruslan <ruslan.olontsev@saritasa.com>
2022-08-23 19:54:04 +0500

    Update to camel case notation

--- database/migrations/2014_10_12_200003_create_user_device_table.php
+++ database/migrations/2014_10_12_200003_create_user_device_table.php
@@ -16,7 +16,7 @@ class CreateUserDeviceTable extends Migration
         Schema::create('UserDevice', function (Blueprint $table) {
             $table->increments('id')->index();
             $table->unsignedInteger('user_id');
-            $table->enum('deviceType_id', ['android', 'ios']);
+            $table->enum('deviceTypeId', ['android', 'ios']);
             $table->string('deviceToken', 500);
             $table->string('hash', 40);
             $table->timestamps();
